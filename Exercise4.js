29ccb4ea1ac1315fe6f5c12b2e04c54bbbbc7117
student <you@example.com>
2018-07-30 13:52:00 -0500

    check in

---
+++ Exercise4.js
@@ -0,0 +1,3 @@
+alert("Its a great day today.");
+var name = prompt("What is your name?:");
+alert("hello" + name);
---
+++ ex.5.js
@@ -0,0 +1,3 @@
+var userbalance = parseint(prompt("enter a number"));
+userbalance = userbalance + 50;
+alert("ive granted 50 dollars to your account. your new balance is:")
